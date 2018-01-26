988b8a0091dc575f082f250f965bdd2b8698524f
patenged <patenged@gmail.com>
2018-01-26 11:21:07 -0500

    Add project files.

---
+++ test stuff/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexOf_ToUpper_ToLower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IndexOf
+            string abc = "abcdefghijklmnopqrstuvwxyz";
+            string letter = Console.ReadLine();
+            Console.WriteLine(abc.IndexOf(letter));
+
+
+            ////ToUpper/ToLower
+            //Console.WriteLine("input in UPPERCASE");
+            //string upper = Console.ReadLine();
+            //Console.WriteLine("input in lowercase");
+            //string lower = Console.ReadLine();
+            //upper = upper.ToLower();
+            //lower = lower.ToUpper();
+            //Console.WriteLine(upper);
+            //Console.WriteLine(lower);
+
+        }
+    }
+}
